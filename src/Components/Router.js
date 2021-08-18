6d62145f95bfebda3a5891c9cb0ceb7a67f52e99
hy <chessg55@gmail.com>
2021-08-18 23:51:09 +0900

    router - hashrouter

---
+++ src/Components/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {HashRouter as Router, Route } from "react-router-dom";
+import Home from "Routes/Home";
+import Shows from "Routes/Shows";
+import Search from "Routes/Search";
+// import Detail from "Routes/Detail";
+
+export default () => (
+   <Router>
+      <>
+         <Route path ="/" exact component={Home} />
+         {/* 어떤 URL에서 해당 Route를 render할 지 지정. */}
+         {/* route는 router 안에서만 지정가능하다. */}
+         <Route path ="/shows" exact component={Shows} />
+            <Route path="/shows/popular" render={() => <h1>popular</h1>} />
+         <Route path ="/search" exact component={Search} />
+         {/* <Route path ="/" exact component={Detail} /> */}
+      </>
+   </Router>
+)
