59fcffa7f5d418e5616ca65feaf27ceb6526475e
hy <chessg55@gmail.com>
2021-08-20 16:51:32 +0900

    networking_ api, axios, db

--- src/api.js
+++ src/api.js
@@ -13,11 +13,33 @@ const api = axios.create({
 export const moviesApi = {
    nowPlaying: () => api.get("movie/now_playing"),
    upcoming: () => api.get("movie/upcoming"),
-   popular: () => api.get("movie/popular")
+   popular: () => api.get("movie/popular"),
+   detail: (id) => api.get(`movie/${id}`, {
+      params: {
+         append_to_response: "videos"
+      }
+   }),
+   search: term => api.get("search/movie", {
+      params: {
+         query: term
+         // url 인코딩이 필요. 
+      }
+   })
 }
 
 export const tvApi = {
    topRated: () => api.get("tv/top_rated"),
    popular: () => api.get("tv/popular"),
-   airingToday: () => api.get("tv/airing_today")
+   airingToday: () => api.get("tv/airing_today"),
+   detail: (id) => api.get(`tv/${id}`, {
+      params: {
+         append_to_response: "videos"
+      }
+   }),
+   search: term => api.get("search/tv", {
+      params: {
+         query: term
+         // url 인코딩이 필요. 
+      }
+   })
 }
