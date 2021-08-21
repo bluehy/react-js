7f36f69582b59625c85f24c76e514e0255b2246e
hy <chessg55@gmail.com>
2021-08-21 20:49:12 +0900

    showspresenter setting

--- src/Routes/Shows/ShowsPresenter.js
+++ src/Routes/Shows/ShowsPresenter.js
@@ -2,7 +2,18 @@ import React from "react";
 import propTypes from "prop-types";
 import styled from "styled-components";
 
-const ShowsPresenter = ({topRated, popular, airingToday, error, loading}) => null;
+import Section from "Components/Section";
+
+const Container = styled.div`
+
+`;
+
+const ShowsPresenter = ({topRated, popular, airingToday, error, loading}) => 
+   loading ? null : <Container>
+      {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show=>show.name)}</Section>}
+      {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show=>show.name)}</Section>}
+      {airingToday && airingToday.length > 0 && <Section title="Airing Today Shows">{airingToday.map(show=>show.name)}</Section>}
+   </Container>;
 
 ShowsPresenter.propTypes = {
    topRated:propTypes.array,
