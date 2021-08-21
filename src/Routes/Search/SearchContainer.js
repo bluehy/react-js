f6e294202b6a62d19fdeb851ab1698d63b369f35
hy <chessg55@gmail.com>
2021-08-21 17:41:31 +0900

    search test

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
 
@@ -5,11 +6,39 @@ export default class extends React.Component{
    state={
       movieResults: null,
       tvResults: null,
-      searchTerm: "",
+      searchTerm: "code",
       loading: true,
       error:null
    }
    
+   componentDidMount(){
+      this.handleSubmit();
+   }
+
+
+   handleSubmit = () => {
+      const { searchTerm } = this.state;
+      if(searchTerm !== ""){
+         this.searchByTerm();
+      }
+   }
+
+   searchByTerm = async(term) => {
+      const { searchTerm } = this.state;
+      try {
+         const movieResults = await moviesApi.search(searchTerm);
+         const showResults = await tvApi.search(searchTerm);
+         console.log(movieResults, showResults);
+         this.setState({loading:true});
+      }catch {
+         this.setState({
+            error: "Can't find results."
+         });
+      }finally {
+         this.setState({ loading: false });
+      }
+   }
+
    render(){
       const {movieResults, tvResults, searchTerm, loading, error} = this.state;
       return <SearchPresenter 
