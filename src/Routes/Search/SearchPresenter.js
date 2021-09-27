8650edf9f95b0ff704f4deee279a6c6501b7b332
hayoung <chessg55@gmail.com>
2021-09-27 16:35:15 +0900

    show_poster, information

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Loader from "Components/Loader";
 import Section from "Components/Section";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
    padding: 0px 20px;
@@ -38,8 +39,26 @@ const SearchPresenter = ({movieResults,
       </Form>
       {loading ? <Loader /> : <>
       {/* 늘 결과값이 있는지 확인하는 과정을 생각할 것 */}
-         {movieResults && movieResults.length > 0 && <Section title="Movie Results">{movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
-         {showResults && showResults.length > 0 && <Section title="Show Results">{showResults.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+         {movieResults && movieResults.length > 0 && <Section title="Movie Results">{movieResults.map(movie => 
+            <Poster 
+               key={movie.id} 
+               id={movie.id} 
+               title={movie.original_title} 
+               imageUrl={movie.poster_path}
+               rating={movie.vote_average}
+               year={movie.release_date && movie.release_date.substring(0,4)}
+               isMovie={true}
+            />
+         )}</Section>}
+         {showResults && showResults.length > 0 && <Section title="Show Results">{showResults.map(show => <Poster 
+               key={show.id} 
+               id={show.id} 
+               title={show.original_name} 
+               imageUrl={show.poster_path}
+               rating={show.vote_average}
+               year={show.first_air_date && show.first_air_date.substring(0,4)}
+            />
+         )}</Section>}
          {error && <Message color="#8e44ad" text={error} /> }
          {movieResults && showResults && movieResults.length === 0 && showResults.length === 0 && <Message text={`Nothing Found for ${searchTerm}`} color="#bdc3c7" />}
       </>}
--- src/Routes/Shows/ShowsPresenter.js
+++ src/Routes/Shows/ShowsPresenter.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
    padding: 0px 20px;
@@ -15,9 +16,36 @@ const ShowsPresenter = ({topRated, popular, airingToday, error, loading}) =>
       <Loader/>
       ) : (
       <Container>
-         {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show=><span key={show.id}>{show.name}</span>)}</Section>}
-         {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show=><span key={show.id}>{show.name}</span>)}</Section>}
-         {airingToday && airingToday.length > 0 && <Section title="Airing Today Shows">{airingToday.map(show=><span key={show.id}>{show.name}</span>)}</Section>}
+         {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show=>
+            <Poster 
+               key={show.id} 
+               id={show.id} 
+               title={show.original_name} 
+               imageUrl={show.poster_path}
+               rating={show.vote_average}
+               year={show.first_air_date && show.first_air_date.substring(0,4)}
+            />
+         )}</Section>}
+         {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show=>
+            <Poster 
+               key={show.id} 
+               id={show.id} 
+               title={show.original_name} 
+               imageUrl={show.poster_path}
+               rating={show.vote_average}
+               year={show.first_air_date && show.first_air_date.substring(0,4)}
+            />
+         )}</Section>}
+         {airingToday && airingToday.length > 0 && <Section title="Airing Today Shows">{airingToday.map(show=>
+            <Poster 
+               key={show.id} 
+               id={show.id} 
+               title={show.original_name} 
+               imageUrl={show.poster_path}
+               rating={show.vote_average}
+               year={show.first_air_date && show.first_air_date.substring(0,4)}
+            />
+         )}</Section>}
          {error && <Message color="#8e44ad" text={error} /> }
       </Container>);
 
